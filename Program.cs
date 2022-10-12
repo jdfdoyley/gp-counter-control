519f60b41fdb7e05eecb85258a1ca137cef7aaeb
Jason D'Oyley <jdfdoyley@gmail.com>
2022-10-12 03:25:31 -0400

    Add initial main class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/******************************************************************************\
+ * Name: Jason D. F. D'Oyley
+ * Date: Oct. 12, 2022
+ * Assignment CIS214 Week 2 GP - Counter Controlled Loops
+ *
+ * Main application (program) class.
+ * In this application we will demonstrate the use of counter-controlled
+ * loops and increment/decrement operators by showing several examples
+ * of counted operations.
+\******************************************************************************/
+public class CounterControlledLoops
+{
+    static void Main(string[] args)
+    {
+        // Code here ...
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# gp-counter-control
+# Counter Controlled Loops
+
+Main application (program) class.
+In this application we will demonstrate the use of counter-controlled
+loops and increment/decrement operators by showing several examples
+of counted operations.
